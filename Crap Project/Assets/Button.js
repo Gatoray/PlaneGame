9cfb7d4fd122e7201dc85da307270b90ef10a7a4
jathman <jathniel.k.manahan@biola.edu>
2014-04-15 12:18:51 -1000

    Rough GUI Layout

---
+++ Crap Project/Assets/Button.js
@@ -0,0 +1,21 @@
+﻿var textAreaString = "Cards";
+
+function OnGUI () 
+{
+	GUI.Box (Rect (10,10,180,330), "Game Menu");
+		
+	textAreaString = GUI.TextArea (Rect (50, 40, 100, 150), textAreaString);
+	
+	
+	if (GUI.Button (Rect (50,200,100,40), "Random Card")) 
+	{
+		Application.LoadLevel (2);
+		print("You Picked a Random Card!");
+	}
+	
+	if (GUI.Button (Rect (20,250,160,80), "PLAY")) 
+	{
+		Application.LoadLevel (1);
+		print("Kill them Nazis!");
+	}
+}
